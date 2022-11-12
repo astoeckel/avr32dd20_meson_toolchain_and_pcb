4007dd3eee11fae1cb298e62c12e73a37f319909
Andreas Stöckel <andreas.stoeckel@googlemail.com>
2022-11-12 01:36:03 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 int main(void) {
     /* This program repeatedly toggles pins PA6 and PA7 with a 180° phase-shift */
     PORTA.DIR = 0b11000000;
-    PORTA.OUT ^= 0b10000000;
+    PORTA.OUT = 0b10000000;
     while (true) {
         PORTA.OUT ^= 0b11000000;
     }
