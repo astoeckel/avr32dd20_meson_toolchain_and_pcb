917b99410a9ca7c4983d1f6b4012b41999679bd8
Andreas Stöckel <andreas.stoeckel@googlemail.com>
2022-11-12 01:34:50 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <avr/io.h>
 
 int main(void) {
+    /* This program repeatedly toggles pins PA6 and PA7 with a 180° phase-shift */
     PORTA.DIR = 0b11000000;
     PORTA.OUT ^= 0b10000000;
     while (true) {
